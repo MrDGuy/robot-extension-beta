720cdaf2e1ea38e6a52b8fd11c52b270d25ef3fc
MrDGuy <84729967+MrDGuy@users.noreply.github.com>
2023-06-21 14:35:04 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -72,15 +72,6 @@ namespace robot {
         . . . . . . . . . . . . . . . 
         `;
 
-  const level1 = tiles.createTilemap(hex`0a000700050607090607060609020e0101010101010110080e0101010101010101080f01010101010101010a0e0101010101010101080d010101010101010108040c0c0b0c0c0c0c0b03`, img`
-2 2 2 2 2 2 2 2 2 2 
-2 . . . . . . . . 2 
-2 . . . . . . . . 2 
-2 . . . . . . . . 2 
-2 . . . . . . . . 2 
-2 . . . . . . . . 2 
-2 2 2 2 2 2 2 2 2 2 
-`, [myTiles.transparency16,myTiles.tile1,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorth1,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterNorth2,sprites.dungeon.greenOuterEast2,sprites.dungeon.greenOuterSouth2,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterWest2,myTiles.tile3], TileScale.Sixteen);
         
     const robotSprite = sprites.create(robotUp), SpriteKind.player)
     //%block
