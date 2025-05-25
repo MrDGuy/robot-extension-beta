b30304aeafb6e78872a03d86c3f7a0a08962e0e9
MrDGuy <84729967+MrDGuy@users.noreply.github.com>
2025-05-25 10:08:56 -0400

    Create tilemap.g.ts

---
+++ tilemap.g.ts
@@ -0,0 +1,9 @@
+// Auto-generated. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const startTile = image.ofBuffer(hex`00000000000000000000000000000000`)
+    //% fixedInstance jres blockIdentity=images._tile
+    export const goalTile = image.ofBuffer(hex`11111111111111111111111111111111`)
+    //% fixedInstance jres blockIdentity=images._tile
+    export const coinTile = image.ofBuffer(hex`22222222222222222222222222222222`)
+}
