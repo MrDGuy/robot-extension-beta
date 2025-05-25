6a8aa56e3106e159bc39d4e1c45d13da66ee5baa
MrDGuy <84729967+MrDGuy@users.noreply.github.com>
2025-05-25 10:19:13 -0400

    Update tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -1,9 +1,23 @@
-// Auto-generated. Do not edit.
+// Auto-generated code. Do not edit.
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
-    export const startTile = image.ofBuffer(hex`00000000000000000000000000000000`)
+    export const tile799 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
-    export const goalTile = image.ofBuffer(hex`11111111111111111111111111111111`)
+    export const tile899 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
-    export const coinTile = image.ofBuffer(hex`22222222222222222222222222222222`)
+    export const tile999 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "startTile":
+            case "tile799":return tile7;
+            case "goalTile":
+            case "tile899":return tile8;
+            case "coinTile":
+            case "tile999":return tile9;
+        }
+        return null;
+    })
+
 }
+// Auto-generated code. Do not edit.
